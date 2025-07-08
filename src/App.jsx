f6abfaa1cd2f4a563e19e1dbf2036f98a7dec7a1
meherab111 <mehrabborno@gmail.com>
2025-07-08 20:26:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { getPost } from "./api/PostApi";
+
+const App = () => {
+  
+  const getPostData = async () => {
+    
+    const response = await getPost()
+
+    console.log(response.data);
+  }
+  
+  useEffect(() => {
+
+    getPostData()
+
+  },[])
+
+  return <h1>react crud op</h1>;
+};
+
+export default App;
